81bdde64fb3b78e491ebd60becd1d59eda8a4a81
Bryan Vazquez <gamer.brybry@gmail.com>
2026-07-27 18:02:11 -0700

    fix: configured next.config.ts for cloudflare (generate URL to test other physical devices)

--- apps/platform/next.config.ts
+++ apps/platform/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allows Next.js developement assets and client-side JavaScript to load
+  // when the app is accessed through a temporary Cloudflare Tunnel URL.
+  allowedDevOrigins: [
+    "*.trycloudflare.com",
+  ],
 };
 
 export default nextConfig;
