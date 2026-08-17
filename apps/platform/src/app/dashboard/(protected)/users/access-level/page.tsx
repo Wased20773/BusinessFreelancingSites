e122b563891ad9a1d5fb42c98c91d64211757c3a
Bryan Vazquez <gamer.brybry@gmail.com>
2026-08-17 16:46:33 -0700

    css: added some background modifications to the dashboard

--- apps/platform/src/app/dashboard/(protected)/users/access-level/page.tsx
+++ apps/platform/src/app/dashboard/(protected)/users/access-level/page.tsx
@@ -1,6 +1,7 @@
 import ArrowIcon from "@/components/icons/arrow";
 import Divider from "@/components/layout/Divider";
 import Link from "next/link";
+import "../../page.css";
 
 export default function AccessLevelsPage() {
   return (
@@ -11,53 +12,57 @@ export default function AccessLevelsPage() {
         </Link>
         <h1 id="access-level-heading">Access Level</h1>
       </div>
-      <p>
-        This defines the permissions a user has in a business. This allows
-        certain users to add, update, or delete content from their business
-        while others cant. Please read the
-        <span className="font-semibold"> Access Level</span> descriptions
-        carefully before making a decision.
-      </p>
 
-      <Divider />
+      <div className="dashboard-card">
+        <p>
+          This defines the permissions a user has in a business. This allows
+          certain users to add, update, or delete content from their business
+          while others cant. Please read the
+          <span className="font-semibold"> Access Level</span> descriptions
+          carefully before making a decision.
+        </p>
 
-      {/* Business.Role: accessLevel, description */}
+        <Divider />
 
-      <article className="flex flex-col gap-3">
-        <div>
-          <p className="font-bold">Developer</p>
-          <p>
-            Responsible for the technical integration of the business website.
-            Can create, view, rotate, deactivate, and delete Business API keys
-            used by the website.
-          </p>
-        </div>
+        {/* Business.Role: accessLevel, description */}
 
-        <div>
-          <p className="font-bold">Owner</p>
-          <p>
-            Full business-level access. Can add, update, and change business
-            content, manage users/roles, and transfer ownership.
-          </p>
-        </div>
+        <article className="flex flex-col gap-3">
+          <div>
+            <p className="font-bold">Developer</p>
+            <p>
+              Responsible for the technical integration of the business website.
+              Can create, view, rotate, deactivate, and delete Business API keys
+              used by the website.
+            </p>
+          </div>
 
-        <div>
-          <p className="font-bold">Admin</p>
-          <p>
-            Can add, update, and delete business content. Can manage general
-            user information, but cannot remove an owner, or transfer ownership.
-          </p>
-        </div>
+          <div>
+            <p className="font-bold">Owner</p>
+            <p>
+              Full business-level access. Can add, update, and change business
+              content, manage users/roles, and transfer ownership.
+            </p>
+          </div>
 
-        <div>
-          <p className="font-bold">Staff</p>
-          <p>
-            View-only access. Can view business information but cannot add,
-            update, or delete business content. Free to update their credentials
-            but not role.
-          </p>
-        </div>
-      </article>
+          <div>
+            <p className="font-bold">Admin</p>
+            <p>
+              Can add, update, and delete business content. Can manage general
+              user information, but cannot remove an owner, or transfer
+              ownership.
+            </p>
+          </div>
+
+          <div>
+            <p className="font-bold">Staff</p>
+            <p>
+              View-only access. Can view business information but cannot add,
+              update, or delete business content. Free to update their
+              credentials but not role.
+            </p>
+          </div>
+        </article>
+      </div>
     </div>
   );
 }
